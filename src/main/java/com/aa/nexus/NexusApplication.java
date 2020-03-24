c1dd6433fc1162a913d3a20938383764bbd9921f
zhangpengfei <563786979@qq.com>
2020-03-24 21:45:40 +0800

    feature1

--- src/main/java/com/aa/nexus/NexusApplication.java
+++ src/main/java/com/aa/nexus/NexusApplication.java
@@ -2,7 +2,10 @@ package com.aa.nexus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class NexusApplication {
 
@@ -10,4 +13,8 @@ public class NexusApplication {
         SpringApplication.run(NexusApplication.class, args);
     }
 
+    @RequestMapping("feature_one")
+    public String one(){
+        return "feature_one";
+    }
 }
